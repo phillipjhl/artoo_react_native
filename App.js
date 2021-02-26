9709f97ffdb0e200177c816630d3af7c4583f7fa
Phillip Langland <Phillip.Langland@chiprewards.com>
2021-02-25 18:00:41 -0600

    updates to get it working

--- App.js
+++ App.js
@@ -1,11 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
   View,
   Text,
-  StatusBar,
 } from 'react-native';
 import { enableScreens } from 'react-native-screens';
 import { NavigationContainer } from '@react-navigation/native';
@@ -24,27 +20,37 @@ function HomeScreen() {
   const [data, setData] = useState({ sensorData: { temp: '', humidity: '' } });
 
   function getData() {
-    fetch('http://192.168.0.115:8000/api/sensors/data/', {
+    fetch('http://192.168.0.115/api/sensors/data/?sensorId=1', {
       headers: {
         Accept: 'application/json',
+        Authorization: `Bearer `
       },
       method: 'GET',
     })
-      .then((resp) => resp.json())
+      .then((resp) => {
+        switch (resp.status) {
+          case 200:
+            return resp.json();
+          default:
+            throw resp;
+        }
+      })
       .then((json) => {
-        console.log(json);
-        setData({
-          sensorData: {
-            temp: json.filter((a, b) => a.name == 'temperature'),
-            humidity: json.filter((a, b) => a.name == 'humidity'),
-          },
-        });
+        let results = json.results;
+        if (results) {
+          setData({
+            sensorData: {
+              temp: results.filter((a, b) => a.name == 'temperature'),
+              humidity: results.filter((a, b) => a.name == 'humidity'),
+            },
+          });
+        }
       }).catch(error => console.error(error));
   }
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Home!</Text>
-      <LineGraph domain={{ y: [20, 30] }} data={data.sensorData.temp} />
+      <LineGraph name={"Temperature"} domain={{ y: [20, 30] }} data={data.sensorData.temp} />
     </View>
   );
 }
--- js/components/Chart.js
+++ js/components/Chart.js
@@ -4,43 +4,42 @@ import {
   VictoryChart,
   VictoryTheme,
   VictoryClipContainer,
-  VictoryAxis,
-} from 'victory';
+} from 'victory-native';
 import moment from 'moment';
 import { View, Text } from 'react-native';
 
 export default function LineGraph(props) {
-  console.log(props);
 
   let dataMap = props.data ? props.data.map((d, i) => {
-    let date = moment(d.created_at).format('MM:SS');
+    let date = moment(d.created_at).unix();
     let map = { x: date, y: parseFloat(d.values) };
     return map;
   }) : [];
 
   return (
-    // <VictoryChart
-    //   domain={props.domain}
-    //   theme={VictoryTheme.material}
-    //   animate={{ duration: 500, onLoad: { duration: 500 } }}
-    //   padding={{ top: 40, bottom: 80, left: 40, right: 80 }}
-    //   width={600}
-    //   height={470}
-    //   scale={{ x: 'time' }}>
-    //   <VictoryLine
-    //     // labels={({ datum }) => `${datum.y}`}
-    //     data={dataMap}
-    //     groupComponent={
-    //       <VictoryClipContainer clipPadding={{ top: 5, right: 10 }} />
-    //     }
-    //     style={{
-    //       data: { stroke: '#c43a31' },
-    //       parent: { border: '1px solid #ccc' },
-    //     }}
-    //   />
-    // </VictoryChart>
-    <View>
-      <Text>Line Graph</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignContent: "center" }}>
+      <VictoryChart
+        name={props.name}
+        domain={props.domain}
+        theme={VictoryTheme.material}
+        animate={{ duration: 500, onLoad: { duration: 500 } }}
+        padding={{ top: 0, bottom: 0, left: 0, right: 0 }}
+        width={600}
+        height={500}
+        scale={{ x: 'time' }}>
+        <VictoryLine
+          // labels={({ datum }) => `${datum.y}`}
+          interpolation="natural"
+          data={dataMap}
+          groupComponent={
+            <VictoryClipContainer clipPadding={{ top: 0, right: 0 }} />
+          }
+          style={{
+            data: { stroke: '#c43a31' },
+            parent: { border: '1px solid #ccc' },
+          }}
+        />
+      </VictoryChart>
     </View>
   );
 }
